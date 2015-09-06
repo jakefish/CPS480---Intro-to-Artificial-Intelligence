116dd322eee544c0bb390e5638ac5ae4a0e560bb
David Fishbaugh <Fishbaugh@Davids-iMac.local>
2015-09-06 18:05:46 -0400

    Report expert system results

--- expert_system/expert_system.py
+++ expert_system/expert_system.py
@@ -1,103 +1,111 @@
 from wow_classes import (warrior, paladin, rogue, priest, death_knight, shaman,
                             mage, warlock, monk, druid, wow_classes)
 
+question_one = raw_input("Are you angered easily? y/n ")
 
-answer = raw_input("Are you angered easily? y/n ")
-
-if answer is 'y':
+if question_one is 'y':
     warrior.class_rank = warrior.class_rank + 1
 
-answer = raw_input("Do you protect the weak? y/n ")
+question_two  = raw_input("Do you protect the weak? y/n ")
 
-if answer is 'y':
+if question_two is 'y':
     paladin.class_rank = paladin.class_rank + 1
 
-answer = raw_input("Are you quick and stealthy? y/n ")
+question_three = raw_input("Are you quick and stealthy? y/n ")
 
-if answer is 'y':
+if question_three is 'y':
     rogue.class_rank = rogue.class_rank + 1
 
-answer = raw_input("Are you spiritual? y/n ")
+question_four  = raw_input("Are you spiritual? y/n ")
 
-if answer is 'y':
+if question_four is 'y':
     priest.class_rank = priest.class_rank + 1
 
-answer = raw_input("Do you use others to do your dirty work? y/n ")
+question_five = raw_input("Do you use others to do your dirty work? y/n ")
 
-if answer is 'y':
+if question_five is 'y':
     death_knight.class_rank = death_knight.class_rank + 1
+print warrior.class_rank
+question_six = raw_input("Do like playing with fire? y/n ")
 
-answer = raw_input("Do like playing with fire? y/n ")
-
-if answer is 'y':
+if question_six is 'y':
     shaman.class_rank = shaman.class_rank + 1
 
-answer = raw_input("Are you gifted with keen intellect? y/n ")
+question_seven = raw_input("Are you gifted with keen intellect? y/n ")
 
-if answer is 'y':
+if question_seven is 'y':
     mage.class_rank = mage.class_rank + 1
 
-answer = raw_input("Do you see opportunity in death? y/n ")
+question_eight = raw_input("Do you see opportunity in death? y/n ")
 
-if answer is 'y':
+if question_eight is 'y':
     warlock.class_rank = warlock.class_rank + 1
 
-answer = raw_input("Are you self disciplined? y/n ")
+question_nine = raw_input("Are you self disciplined? y/n ")
 
-if answer is 'y':
+if question_nine is 'y':
     monk.class_rank = monk.class_rank + 1
 
-answer = raw_input("Do you love animals? y/n ")
+question_ten = raw_input("Do you love animals? y/n ")
 
-if answer is 'y':
+if question_ten is 'y':
     druid.class_rank = druid.class_rank + 1
 
-answer = raw_input("Are you a protector of nature? y/n ")
+question_eleven = raw_input("Are you a protector of nature? y/n ")
 
-if answer is 'y':
+if question_eleven is 'y':
     druid.class_rank = druid.class_rank + 1
 
-answer = raw_input("Do you pummel your foes with your fists? y/n ")
+question_twelve = raw_input("Do you pummel your foes with your fists? y/n ")
 
-if answer is 'y':
+if question_twelve is 'y':
     monk.class_rank = monk.class_rank + 1
 
-answer = raw_input("Is dominance your aim? y/n ")
+question_thirteen = raw_input("Is dominance your aim? y/n ")
 
-if answer is 'y':
+if question_thirteen is 'y':
     warlock.class_rank = warlock.class_rank + 1
 
-answer = raw_input("Do you enjoy reading book? y/n ")
+question_fourteen = raw_input("Do you enjoy reading book? y/n ")
 
-if answer is 'y':
+if question_fourteen is 'y':
     mage.class_rank = mage.class_rank + 1
 
-answer = raw_input("Do you consider yourself a spiritual leader? y/n ")
+question_fifteen = raw_input("Do you consider yourself a spiritual leader? y/n ")
 
-if answer is 'y':
+if question_fifteen is 'y':
     shaman.class_rank = shaman.class_rank + 1
 
-answer = raw_input("Do you fight with axes and one-handed weapons? y/n ")
+question_sixteen = raw_input("Do you fight with axes and one-handed weapons? y/n ")
 
-if answer is 'y':
+if question_sixteen is 'y':
     death_knight.class_rank = death_knight.class_rank + 1
 
-answer = raw_input("Do you have an unwaivering faith to serve people? y/n ")
+question_seventeen = raw_input("Do you have an unwaivering faith to serve people? y/n ")
 
-if answer is 'y':
+if question_seventeen is 'y':
     priest.class_rank = priest.class_rank + 1
 
-answer = raw_input("Do you gain honor from gold? y/n ")
+question_eightteen = raw_input("Do you gain honor from gold? y/n ")
 
-if answer is 'y':
+if question_eightteen is 'y':
     rogue.class_rank = rogue.class_rank + 1
 
-answer = raw_input("Do you value justice and vanquish evil? y/n ")
+question_nineteen = raw_input("Do you value justice and vanquish evil? y/n ")
 
-if answer is 'y':
+if question_nineteen is 'y':
     paladin.class_rank = paladin.class_rank + 1
 
-answer = raw_input("Do you you find yourself taking the blame for something you didn't do? y/n ")
+question_twenty = raw_input("Do you you find yourself taking the blame for something you didn't do? y/n ")
 
-if answer is 'y':
+if question_twenty is 'y':
     warrior.class_rank = warrior.class_rank + 1
+
+
+highest_rank = 0
+for wow_class in wow_classes:
+    if wow_class.class_rank >= highest_rank:
+        highest_rank = wow_class.class_rank
+        winning_class = wow_class.class_name
+
+print "Based on your answers you should play as a {0}".format(winning_class)
---
+++ expert_system/util.py
@@ -0,0 +1,3 @@
+def calculate_results(wow_classes):
+    for w in wow_classes:
+        print w.class_name
