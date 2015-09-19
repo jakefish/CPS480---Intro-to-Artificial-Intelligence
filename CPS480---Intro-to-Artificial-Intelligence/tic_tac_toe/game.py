b7823cb2b66d54f4d7aef485378923f52e48f715
Jake Fish <jacobwfish@gmail.com>
2015-09-19 14:40:55 -0400

    Add more base functions to be used in game

--- CPS480---Intro-to-Artificial-Intelligence/tic_tac_toe/game.py
+++ CPS480---Intro-to-Artificial-Intelligence/tic_tac_toe/game.py
@@ -1,20 +1,63 @@
 class Game:
 
     def __init__(self):
-        pass
-    def draw_board(self, board):
+        self.board = [' '] * 10
+
+    def draw_board(self):
         print('   |   |')
-        print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-        print('   |   |')
-        print('-----------')
-        print('   |   |')
-        print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+        print(' ' + self.board[7] + ' | ' + self.board[8] + ' | ' + self.board[9])
         print('   |   |')
         print('-----------')
         print('   |   |')
-        print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+        print(' ' + self.board[4] + ' | ' + self.board[5] + ' | ' + self.board[6])
         print('   |   |')
-        pass
+        print('-----------')
+        print('   |   |')
+        print(' ' + self.board[1] + ' | ' + self.board[2] + ' | ' + self.board[3])
+        print('   |   |')
+
+    def choose_player_letter(self):
+        player_letter = raw_input('Which you like to play as X or O?')
+        player_letter = player_letter.upper()
+        print player_letter
+
+        if player_letter is 'X':
+            return ['X', 'O']
+        else:
+            return ['O', 'X']
+
+    def determine_who_plays_first(self):
+        import random
+        random_player = ['computer', 'player']
+        return random_player[random.randint(0, 1)]
+
+    def make_a_move(board, letter, move):
+        self.board[move] = letter
+
+    def check_for_winner(self, letter):
+        if self.board[7] == letter and self.board[8] == letter and self.board[9] == letter
+            winner = True
+        elif self.board[4] == letter and self.board[5] == letter and self.board[6] == letter:
+            winner = True
+        elif self.board[1] == letter and self.board[2] == letter and self.board[3] == letter:
+            winner = True
+        elif self.board[7] == letter and self.board[4] == letter and self.board[1] == letter:
+            winner = True
+        elif self.board[8] == letter and self.board[5] == letter and self.board[2] == letter:
+            winner = True
+        elif self.board[9] == letter and self.board[6] == letter and self.board[3] == letter:
+            winner = True
+        elif self.board[7] == letter and self.board[5] == letter and self.board[3] == letter:
+            winner = True
+        elif self.board[9] == letter and self.board[5] == letter and self.board[3] == letter:
+            winner = True
+        else:
+            winner = False
+        return winner
+
+
+
+
 
 
 
@@ -22,6 +65,6 @@ board = [' '] * 10
 
 game = Game()
 
-game.draw_board(board)
-
-print board
+game.draw_board()
+game.choose_player_letter()
+print game.determine_who_plays_first()
